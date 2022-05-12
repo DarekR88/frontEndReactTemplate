9acbafd95223e3bda896cd324582e6708e19a540
Darek Radke <dradmclad@gmail.com>
2022-05-12 18:16:41 -0500

    create-react-app and components folder

---
+++ frontendtemplate/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const toolbar = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div>THE LOGO</div>
+            <div>
+                <ul>
+                    <li>products</li>
+                    <li>users</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
